f7f1c31e7fa28d2b1dec46df65f0d0eba31dc3e4
Roberto Bösenberg <44812635+roboes@users.noreply.github.com>
2023-10-10 23:55:39 +0200

    Update gps_tools.sh

--- sports/gps_tools.sh
+++ sports/gps_tools.sh
@@ -1,5 +1,5 @@
 ## GPS Tools
-# Last update: 2023-08-12
+# Last update: 2023-10-10
 
 
 # Rename: ExifTool
@@ -36,6 +36,17 @@ for file in ./*.gpx; do
 	gpsbabel -t -i gpx -f "$file" -x track,faketime=f20220605200000+2 -o gtrnctr,course=0  -F "${file%.*}.tcx"
 	echo ${file%.*}
 done
+
+
+# Combine multiple .tcx activity files into one .tcx file (for bulk upload to Strava - Strava will automatically separate/split these activities after upload - does not work with combined activity files of type .gpx and .fit)
+files=""
+for file in ./*.tcx; do
+	files="$files -f $file"
+done
+
+gpsbabel -t -r -w -i gtrnctr $files -o gtrnctr,course=0 -F activities_combined.tcx # .tcx activity files
+# gpsbabel -t -r -w -i gpx $files -o gtrnctr,course=0 -F activities_combined.tcx # .gpx activity files
+# gpsbabel -t -r -w -i garmin_fit $files -o gtrnctr,course=0 -F activities_combined.tcx # .fit activity files
 
 
 # Merge two .fit files (including heart rate data) into a single .gpx file for Strava upload (order matters)
