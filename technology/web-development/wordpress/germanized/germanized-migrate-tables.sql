d9630ab2edd6fafa09c9bf55e6892fbf2019292f
roboes <rg.roberto@outlook.com>
2024-06-23 18:59:58 +0200

    Update

--- technology/web-development/wordpress/germanized/germanized-migrate-tables.sql
+++ technology/web-development/wordpress/germanized/germanized-migrate-tables.sql
@@ -1,5 +1,5 @@
 /* Germanized - Migrate WooCommerce Germanized tables from one dataset to another */
-/* Last update: 2024-06-20 */
+/* Last update: 2024-06-23 */
 
 
 /* Important: Import the Germanized tables before activating the shop in the new dataset - otherwise the "meta_id" could ver overlapped. */
@@ -17,10 +17,10 @@ wp_woocommerce_gzd_shipments */
 /* Step 1 - Export the "wp_woocommerce_gzd_shipment_itemmeta" table from the "old" dataset without selecting "Add CREATE DATABASE / USE statement". Open the exported .sql file and edit the name of the table to "wp_woocommerce_gzd_shipment_itemmeta_old". Import it to the new dataset. */
 
 
-/* Step 2 - Test if "meta_id" column is unique (Non_unique = 0 and Key_name = Primary) */
-SHOW INDEXES FROM wp_woocommerce_gzd_shipment_itemmeta WHERE Column_name='meta_id' AND NOT Non_unique;
+/* Step 2 - Test if "meta_id" column is unique (NON_UNIQUE = 0 and INDEX_NAME = Primary) */
+SELECT * FROM information_schema.statistics WHERE table_schema = 'wordpress' AND table_name = 'wp_woocommerce_gzd_shipment_itemmeta' AND column_name = 'meta_id' AND non_unique = 0;
 
-SHOW INDEXES FROM wp_woocommerce_gzd_shipment_itemmeta_old WHERE Column_name='meta_id' AND NOT Non_unique;
+SELECT * FROM information_schema.statistics WHERE table_schema = 'wordpress' AND table_name = 'wp_woocommerce_gzd_shipment_itemmeta_old' AND column_name = 'meta_id' AND non_unique = 0;
 
 
 /* Step 3: Create a temporary table for testing purposes */
@@ -32,11 +32,11 @@ SELECT * FROM wp_woocommerce_gzd_shipment_itemmeta;
 
 /* Step 4: Update the new table with data from the old table */
 UPDATE temp_wp_woocommerce_gzd_shipment_itemmeta AS new
-JOIN wp_woocommerce_gzd_shipment_itemmeta_old AS old
-ON new.meta_id = old.meta_id
+INNER JOIN wp_woocommerce_gzd_shipment_itemmeta_old AS old
+    ON new.meta_id = old.meta_id
 SET
-  new.gzd_shipment_item_id = old.gzd_shipment_item_id,
-  new.meta_value = old.meta_value;
+    new.gzd_shipment_item_id = old.gzd_shipment_item_id,
+    new.meta_value = old.meta_value;
 
 
 /* Optional Step: Insert non-matching rows from old table */
