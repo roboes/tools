620ee287d36f2876d7ed5624db686a93c91243ed
roboes <rg.roberto@outlook.com>
2025-06-29 18:01:22 +0200

    Update

---
+++ technology/microsoft/windows/windows-subsystem-for-linux-packages.sh
@@ -0,0 +1,36 @@
+## Windows Subsystem for Linux Packages
+# Last update: 2025-05-14
+
+
+# Homebrew update
+ulimit -n 8192
+brew update && brew upgrade && brew cleanup
+
+
+# Install GitHub CLI
+# brew install git
+# brew install gh
+
+# Install GitLab Runner
+# brew install gitlab-runner
+
+
+# GitHub login
+# gh auth setup-git
+# gh auth login
+
+# Display git custom settings
+# git config --list
+
+# Remove git custom settings
+# rm ~/.gitconfig
+
+
+# Git settings
+# git config --global user.email "email@example.com"
+# git config --global user.name "username"
+# git config --global --list
+
+
+# Git ignore
+# https://github.com/github/gitignore/blob/main/Python.gitignore
