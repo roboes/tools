b5536e6ab69656987b46f0883c9fab7b1c77967f
roboes <rg.roberto@outlook.com>
2025-07-22 10:45:18 +0200

    Update

--- technology/web-development/wordpress/woocommerce-test/woocommerce-test-meta-information.php
+++ technology/web-development/wordpress/woocommerce-test/woocommerce-test-meta-information.php
@@ -1,7 +1,7 @@
 <?php
 
 // WooCommerce Test - Order meta information
-// Last update: 2024-08-27
+// Last update: 2025-07-14
 
 
 // Settings
@@ -10,7 +10,7 @@ $order = wc_get_order($order_id);
 
 
 if ($order) {
-    $order_meta = get_post_meta($order_id);
+    $order_meta = $order->get_meta_data();
     echo '<pre>';
     print_r($order_meta);
     echo '</pre>';
@@ -23,17 +23,6 @@ if ($order) {
     $order_data = $order->get_data();
     echo '<pre>';
     print_r($order_data);
-    echo '</pre>';
-} else {
-    echo 'Order not found.';
-}
-
-
-# Accessing Order Meta Data with High-Performance Order Storage (HPOS)
-if ($order) {
-    $order_meta = $order->get_meta_data();
-    echo '<pre>';
-    print_r($order_meta);
     echo '</pre>';
 } else {
     echo 'Order not found.';
